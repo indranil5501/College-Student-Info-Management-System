cb5eae76f565da390a880dc37af061dfe9d0d3a8
Indranil Das <61935696+indranil5501@users.noreply.github.com>
2021-05-31 22:29:20 +0530

    Add files via upload

---
+++ stdinfoman/src/javaproject/StudentBean.java
@@ -0,0 +1,62 @@
+package javaproject;
+
+/**
+ *
+ * @author Y.H.Bhosale
+ */
+public class StudentBean {
+    private int id;
+    private String name;
+    private float fees;
+    private String dob;
+    private byte[] photo;
+    
+    public StudentBean(int id,String name,float fees,String dob,byte[] photo){
+        this.id=id;
+        this.name=name;
+        this.fees=fees;
+        this.dob=dob;
+        this.photo=photo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getFees() {
+        return fees;
+    }
+
+    public void setFees(float fees) {
+        this.fees = fees;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public byte[] getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(byte[] photo) {
+        this.photo = photo;
+    }   
+    
+} 
